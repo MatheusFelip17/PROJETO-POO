6d609010f2ff74c84055fe582a31ea2f352fb84c
MatheusFelip17 <mf3562416@gmail.com>
2026-07-06 16:36:30 -0300

    commit 5

--- Principal/sistema.py
+++ Principal/sistema.py
@@ -1,20 +1,63 @@
 import tkinter as tk
+from tkinter import ttk
 
 def abrir():
     janela = tk.Tk()
-
     janela.title("Sistema")
     janela.geometry("980x680")
-    janela.minsize(820, 560)
     janela.configure(bg="#1E293B")
 
-    texto = tk.Label(
-        janela,
-        text="Bem-vindo ao Sistema da Mecânica do Zé!",
-        font=("Arial", 14),
-        bg="#1E293B",
-        fg="#E2E8F0"
-    )
-    texto.pack(expand=True)
+    abas = ttk.Notebook(janela)
+    abas.pack(expand=True, fill="both")
+
+    aba1 = tk.Frame(abas)
+    aba2 = tk.Frame(abas)
+
+    abas.add(aba1, text="Depósito")
+    abas.add(aba2, text="Tabela de preços")
+
+    Tabela = ttk.Treeview(
+    aba1,
+    columns=("codigo", "nome", "quantidade"),
+    show="headings"
+)
+    
+    Tabela.heading("codigo", text="Código")
+    Tabela.heading("nome", text="Descrição")
+    Tabela.heading("quantidade", text="Quantidade")
+
+    Tabela.column("codigo", width=60, anchor="center")
+    Tabela.column("nome", width=200, anchor="center")
+    Tabela.column("quantidade", width=80, anchor="center")
+
+    Tabela.insert("", tk.END, values=("001", "Filtro de óleo", 50))
+    Tabela.insert("", tk.END, values=("002", "Pastilha de freio dianteira", 50))
+    Tabela.insert("", tk.END, values=("003", "Amortecedor dianteiro", 50))
+    Tabela.insert("", tk.END, values=("004", "Vela de ignição", 50))
+
+    Tabela.pack(expand=True, fill="both", padx=10, pady=10)
+
+    tabela = ttk.Treeview(
+    aba2,
+    columns=("codigo", "nome", "preco", "localizacao"),
+    show="headings"
+)
+    
+    tabela.heading("codigo", text="Código")
+    tabela.heading("nome", text="Descrição")
+    tabela.heading("preco", text="Preço(R$)")
+    tabela.heading("localizacao", text="Localização")
+
+    tabela.column("codigo", width=60, anchor="center")
+    tabela.column("nome", width=200, anchor="center")
+    tabela.column("preco", width=80, anchor="center")
+    tabela.column("localizacao", width=150, anchor="center")
+
+    tabela.insert("", tk.END, values=("001", "Filtro de óleo", "32.50", "Setor A1"))
+    tabela.insert("", tk.END, values=("002", "Pastilha de freio dianteira", "139.90", "Setor C2"))
+    tabela.insert("", tk.END, values=("003", "Amortecedor dianteiro", "329.90", "Setor C1"))
+    tabela.insert("", tk.END, values=("004", "Vela de ignição", "29.90", "Setor C1"))
+
+    tabela.pack(expand=True, fill="both", padx=10, pady=10)
 
     janela.mainloop()
